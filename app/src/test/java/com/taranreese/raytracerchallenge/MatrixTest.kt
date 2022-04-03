62660e9046820e65fa81e155cbf43b21efd20366
Taran Reese <taranreese@icloud.com>
2022-04-03 00:28:59 -0400

    more reformatting

--- app/src/test/java/com/taranreese/raytracerchallenge/MatrixTest.kt
+++ app/src/test/java/com/taranreese/raytracerchallenge/MatrixTest.kt
@@ -17,100 +17,119 @@ class MatrixTest {
             )
         )
 
-        assertEquals(m[0,0], 1.0)
-        assertEquals(m[0,3], 4.0)
-        assertEquals(m[1,0], 5.5)
-        assertEquals(m[1,2], 7.5)
-        assertEquals(m[2,2], 11.0)
-        assertEquals(m[3,0], 13.5)
-        assertEquals(m[3,2], 15.5)
+        assertEquals(m[0, 0], 1.0)
+        assertEquals(m[0, 3], 4.0)
+        assertEquals(m[1, 0], 5.5)
+        assertEquals(m[1, 2], 7.5)
+        assertEquals(m[2, 2], 11.0)
+        assertEquals(m[3, 0], 13.5)
+        assertEquals(m[3, 2], 15.5)
     }
 
     @Test
     fun test2x2Constructor() {
-        val m = Matrix2(values = doubleArrayOf(
-            -3.0, 5.0,
-            1.0, -2.0
-        ))
+        val m = Matrix2(
+            values = doubleArrayOf(
+                -3.0, 5.0,
+                1.0, -2.0
+            )
+        )
 
-        assertEquals(m[0,0], -3.0)
-        assertEquals(m[0,1], 5.0)
-        assertEquals(m[1,0], 1.0)
-        assertEquals(m[1,1], -2.0)
+        assertEquals(m[0, 0], -3.0)
+        assertEquals(m[0, 1], 5.0)
+        assertEquals(m[1, 0], 1.0)
+        assertEquals(m[1, 1], -2.0)
     }
 
     @Test
     fun test3x3Constructor() {
-        val m = Matrix3(values = doubleArrayOf(
-            -3.0, 5.0, 0.0,
-            1.0, -2.0, -7.0,
-            0.0, 1.0, 1.0))
+        val m = Matrix3(
+            values = doubleArrayOf(
+                -3.0, 5.0, 0.0,
+                1.0, -2.0, -7.0,
+                0.0, 1.0, 1.0
+            )
+        )
 
-        assertEquals(m[0,0], -3.0)
-        assertEquals(m[1,1], -2.0)
-        assertEquals(m[2,2], 1.0)
+        assertEquals(m[0, 0], -3.0)
+        assertEquals(m[1, 1], -2.0)
+        assertEquals(m[2, 2], 1.0)
     }
 
     @Test
     fun testMatrixEquality_identicalMatrices() {
-        val m1 = Matrix4(values = doubleArrayOf(
-            1.0, 2.0, 3.0, 4.0,
-            5.0, 6.0, 7.0, 8.0,
-            9.0, 8.0, 7.0, 6.0,
-            5.0, 4.0, 3.0, 2.0
-        ))
+        val m1 = Matrix4(
+            values = doubleArrayOf(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 8.0, 7.0, 6.0,
+                5.0, 4.0, 3.0, 2.0
+            )
+        )
 
-        val m2 = Matrix4(values = doubleArrayOf(
-            1.0, 2.0, 3.0, 4.0,
-            5.0, 6.0, 7.0, 8.0,
-            9.0, 8.0, 7.0, 6.0,
-            5.0, 4.0, 3.0, 2.0
-        ))
+        val m2 = Matrix4(
+            values = doubleArrayOf(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 8.0, 7.0, 6.0,
+                5.0, 4.0, 3.0, 2.0
+            )
+        )
 
         assertEquals(m1, m2)
     }
 
     @Test
     fun testMatrixEquality_differentMatrices() {
-        val m1 = Matrix4(values = doubleArrayOf(
-            1.0, 2.0, 3.0, 4.0,
-            5.0, 6.0, 7.0, 8.0,
-            9.0, 8.0, 7.0, 6.0,
-            5.0, 4.0, 3.0, 2.0
-        ))
+        val m1 = Matrix4(
+            values = doubleArrayOf(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 8.0, 7.0, 6.0,
+                5.0, 4.0, 3.0, 2.0
+            )
+        )
 
-        val m2 = Matrix4(values = doubleArrayOf(
-            2.0, 3.0, 4.0, 5.0,
-            6.0, 7.0, 8.0, 9.0,
-            8.0, 7.0, 6.0, 5.0,
-            4.0, 3.0, 2.0, 1.0
-        ))
+        val m2 = Matrix4(
+            values = doubleArrayOf(
+                2.0, 3.0, 4.0, 5.0,
+                6.0, 7.0, 8.0, 9.0,
+                8.0, 7.0, 6.0, 5.0,
+                4.0, 3.0, 2.0, 1.0
+            )
+        )
 
         assertNotSame(m1, m2)
     }
 
     @Test
     fun testMultiplyingMatrices() {
-        val m1 = Matrix4(values = doubleArrayOf(
-            1.0, 2.0, 3.0, 4.0,
-            5.0, 6.0, 7.0, 8.0,
-            9.0, 8.0, 7.0, 6.0,
-            5.0, 4.0, 3.0, 2.0
-        ))
+        val m1 = Matrix4(
+            values = doubleArrayOf(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 8.0, 7.0, 6.0,
+                5.0, 4.0, 3.0, 2.0
+            )
+        )
 
-        val m2 = Matrix4(values = doubleArrayOf(
-            -2.0, 1.0, 2.0, 3.0,
-            3.0, 2.0, 1.0, -1.0,
-            4.0, 3.0, 6.0, 5.0,
-            1.0, 2.0, 7.0, 8.0
-        ))
+        val m2 = Matrix4(
+            values = doubleArrayOf(
+                -2.0, 1.0, 2.0, 3.0,
+                3.0, 2.0, 1.0, -1.0,
+                4.0, 3.0, 6.0, 5.0,
+                1.0, 2.0, 7.0, 8.0
+            )
+        )
 
-        val m3 = Matrix4(values = doubleArrayOf(
-            20.0, 22.0, 50.0, 48.0,
-            44.0, 54.0, 114.0, 108.0,
-            40.0, 58.0, 110.0, 102.0,
-            16.0, 26.0, 46.0, 42.0
-        ))
+        val m3 = Matrix4(
+            values = doubleArrayOf(
+                20.0, 22.0, 50.0, 48.0,
+                44.0, 54.0, 114.0, 108.0,
+                40.0, 58.0, 110.0, 102.0,
+                16.0, 26.0, 46.0, 42.0
+            )
+        )
 
         assertEquals((m1 * m2).matrix, m3.matrix)
     }
@@ -152,19 +171,23 @@ class MatrixTest {
 
     @Test
     fun testTransposingMatrix() {
-        val a = Matrix4(values = doubleArrayOf(
-            0.0, 9.0, 3.0, 0.0,
-            9.0, 8.0, 0.0, 8.0,
-            1.0, 8.0, 5.0, 3.0,
-            0.0, 0.0, 5.0, 8.0
-        ))
+        val a = Matrix4(
+            values = doubleArrayOf(
+                0.0, 9.0, 3.0, 0.0,
+                9.0, 8.0, 0.0, 8.0,
+                1.0, 8.0, 5.0, 3.0,
+                0.0, 0.0, 5.0, 8.0
+            )
+        )
 
-        val b = Matrix4(values = doubleArrayOf(
-            0.0, 9.0, 1.0, 0.0,
-            9.0, 8.0, 8.0, 0.0,
-            3.0, 0.0, 5.0, 5.0,
-            0.0, 8.0, 3.0, 8.0
-        ))
+        val b = Matrix4(
+            values = doubleArrayOf(
+                0.0, 9.0, 1.0, 0.0,
+                9.0, 8.0, 8.0, 0.0,
+                3.0, 0.0, 5.0, 5.0,
+                0.0, 8.0, 3.0, 8.0
+            )
+        )
 
         assertEquals(a.transpose(), b)
     }
@@ -177,44 +200,54 @@ class MatrixTest {
 
     @Test
     fun testDeterminantOfMatrix2() {
-        val a = Matrix2(values = doubleArrayOf(
-            1.0, 5.0,
-            -3.0, 2.0
-        ))
+        val a = Matrix2(
+            values = doubleArrayOf(
+                1.0, 5.0,
+                -3.0, 2.0
+            )
+        )
 
         assertEquals(a.determinant(), 17.0)
     }
 
     @Test
     fun testSubmatrixOf3x3is2x2() {
-        val a = Matrix3(values = doubleArrayOf(
-            1.0, 5.0, 0.0,
-            -3.0, 2.0, 7.0,
-            0.0, 6.0, -3.0
-        ))
+        val a = Matrix3(
+            values = doubleArrayOf(
+                1.0, 5.0, 0.0,
+                -3.0, 2.0, 7.0,
+                0.0, 6.0, -3.0
+            )
+        )
 
-        val b = Matrix2(values = doubleArrayOf(
-            -3.0, 2.0,
-            0.0, 6.0
-        ))
+        val b = Matrix2(
+            values = doubleArrayOf(
+                -3.0, 2.0,
+                0.0, 6.0
+            )
+        )
 
         assertEquals(a.submatrix(0, 2), b)
     }
 
     @Test
     fun testSubmatrixOf4x4is3x3() {
-        val a = Matrix4(values = doubleArrayOf(
-            -6.0, 1.0, 1.0, 6.0,
-            -8.0, 5.0, 8.0, 6.0,
-            -1.0, 0.0, 8.0, 2.0,
-            -7.0, 1.0, -1.0, 1.0
-        ))
+        val a = Matrix4(
+            values = doubleArrayOf(
+                -6.0, 1.0, 1.0, 6.0,
+                -8.0, 5.0, 8.0, 6.0,
+                -1.0, 0.0, 8.0, 2.0,
+                -7.0, 1.0, -1.0, 1.0
+            )
+        )
 
-        val b = Matrix3(values = doubleArrayOf(
-            -6.0, 1.0, 6.0,
-            -8.0, 8.0, 6.0,
-            -7.0, -1.0, 1.0
-        ))
+        val b = Matrix3(
+            values = doubleArrayOf(
+                -6.0, 1.0, 6.0,
+                -8.0, 8.0, 6.0,
+                -7.0, -1.0, 1.0
+            )
+        )
 
         assertEquals(a.submatrix(2, 1), b)
     }
